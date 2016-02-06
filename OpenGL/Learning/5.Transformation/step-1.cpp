8ebb565baadfefe2d32dba62eafe4669ea2f6fff
BLumia <wzc782970009@qq.com>
2016-02-06 12:20:41 +0800

    Lesson 5 Transformation

---
+++ OpenGL/Learning/5.Transformation/step-1.cpp
@@ -0,0 +1,16 @@
+//g++ test.cpp -std=c++11 -o run
+ 
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+using namespace std;
+
+int main() {
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans;
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
+	std::cout << vec.x << vec.y << vec.z << std::endl;
+}
