e11fc0967000cf14dc7d5d29eccefe8b88ab8137
BLumia <wzc782970009@qq.com>
2016-03-07 23:47:40 +0800

    openGL-learning LightCaster step1

---
+++ OpenGL/Learning/12.LightCasters/step-1/fragmentshader.glsl
@@ -0,0 +1,54 @@
+#version 330 core
+
+struct Material
+{
+	sampler2D diffuse;
+	sampler2D specular;
+	//vec3 ambient;
+	//vec3 specular;
+	float shininess;
+};
+uniform Material material;
+
+struct Light
+{
+	//vec3 position;
+	vec3 direction; //平行光
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+uniform Light light;
+
+in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
+out vec4 color;
+
+//uniform vec3 objectColor;
+//uniform vec3 lightColor;
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+void main()
+{
+	// 环境光 = 物体贴图颜色x光照亮度
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+
+	// 漫反射光
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(-light.direction);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+
+	// 镜面高光
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm);  
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	//vec3 specular = light.specular * (spec * material.specular); 
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+	vec3 result = ambient + diffuse + specular;
+	color = vec4(result, 1.0f);
+}
+
